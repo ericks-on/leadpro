3d3d0f75334753f0ac5387e96bad85e62ac4fe92
Erickson Mbuvi <mutisyaerickson@gmail.com>
2024-08-22 15:40:02 +0300

    responsiveness

--- app/inventory/layout.tsx
+++ app/inventory/layout.tsx
@@ -1,4 +1,10 @@
-import { BuildingStorefrontIcon, CurrencyDollarIcon, HomeIcon, ShoppingBagIcon, ShoppingCartIcon } from "@heroicons/react/24/outline";
+import {
+    BuildingStorefrontIcon,
+    CurrencyDollarIcon,
+    HomeIcon,
+    ShoppingBagIcon,
+    ShoppingCartIcon
+} from "@heroicons/react/24/outline";
 import clsx from "clsx";
 import Link from "next/link";
 
@@ -10,32 +16,34 @@ export default function Layout({
     return (
             <div>
                 <div className='flex flex-col md:flex-row basis-1/5'>
-                    <aside className="w-64 h-screen bg-gray-800 text-white flex flex-col items-start py-8 px-6 border-r border-gray-700">
-                        <div className="text-xl font-bold mb-6 px-4">LeadPro Bakers</div>
-                    <nav className={clsx(
-                        "md:flex flex-col w-full",
-                        {showMenu :'fixed width-full h-screen'}
-                    )}>
-                        <Link href="/welcome" className="w-full mb-3 flex gap-2 text-lg font-semibold py-2 px-4 rounded-md hover:bg-gray-700 transition-colors">
-                            <HomeIcon className="w-6 h-6 mr-2" />
-                            Home
-                        </Link>
-                        <Link href="/inventory/products" className="w-full mb-3 flex gap-2 text-lg font-semibold py-2 px-4 rounded-md hover:bg-gray-700 transition-colors">
-                            <ShoppingCartIcon className="w-6 h-6 mr-2" />
-                            Products
-                        </Link>
-                        <Link href="/inventory/sales" className="w-full mb-3 flex gap-2 text-lg font-semibold py-2 px-4 rounded-md hover:bg-gray-700 transition-colors">
-                            <CurrencyDollarIcon className="w-6 h-6 mr-2" />
-                            Sales
-                        </Link>
-                        <Link href="/inventory/kitchen" className="w-full mb-3 text-lg flex gap-2 font-semibold py-2 px-4 rounded-md hover:bg-gray-700 transition-colors">
-                            <BuildingStorefrontIcon className="w-6 h-6 mr-2" />
-                            Kitchen
-                        </Link>
-                        <Link href="/inventory/supplies" className="w-full mb-3 flex gap-2 text-lg font-semibold py-2 px-4 rounded-md hover:bg-gray-700 transition-colors">
-                            <ShoppingBagIcon className="w-6 h-6 mr-2" />
-                            Supplies
-                        </Link>
+                    <aside className=" w-full h-20 md:w-64 md:h-screen bg-gray-800 text-white flex flex-row md:flex-col items-start py-8 px-6 border-r border-gray-700">
+                        <div className="text-xl font-bold mb-6 px-4 hidden md:block">LeadPro Bakers</div>
+                        <nav className="flex flex-row md:flex-col w-full gap-2">
+                            <Link href="/welcome" className="w-full mb-3 flex gap-2 text-lg font-semibold py-2 px-4 rounded-md hover:bg-gray-700 transition-colors">
+                                <HomeIcon className="w-6 h-6 mr-2" />
+                                <span className="hidden md:block">Products</span>
+                                
+                            </Link>
+                            <Link href="/inventory/products" className="w-full mb-3 flex gap-2 text-lg font-semibold py-2 px-4 rounded-md hover:bg-gray-700 transition-colors">
+                                <ShoppingCartIcon className="w-6 h-6 mr-2" />
+                                <span className="hidden md:block">Products</span>
+                                
+                            </Link>
+                            <Link href="/inventory/sales" className="w-full mb-3 flex gap-2 text-lg font-semibold py-2 px-4 rounded-md hover:bg-gray-700 transition-colors">
+                                <CurrencyDollarIcon className="w-6 h-6 mr-2" />
+                                <span className="hidden md:block">Sales</span>
+                                
+                            </Link>
+                            <Link href="/inventory/kitchen" className="w-full mb-3 text-lg flex gap-2 font-semibold py-2 px-4 rounded-md hover:bg-gray-700 transition-colors">
+                                <BuildingStorefrontIcon className="w-6 h-6 mr-2" />
+                                <span className="hidden md:block">Kitchen</span>
+                                
+                            </Link>
+                            <Link href="/inventory/supplies" className="w-full mb-3 flex gap-2 text-lg font-semibold py-2 px-4 rounded-md hover:bg-gray-700 transition-colors">
+                                <ShoppingBagIcon className="w-6 h-6 mr-2" />
+                                <span className="hidden md:block">Supplies</span>
+                                
+                            </Link>
                         </nav>
                     </aside>
                     <div className="basis-4/5">
--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -1,3 +1,10 @@
+import {
+    BuildingStorefrontIcon,
+    CurrencyDollarIcon,
+    HomeIcon,
+    ShoppingBagIcon,
+    ShoppingCartIcon
+} from "@heroicons/react/24/outline";
 import Image from 'next/image';
 import Link from 'next/link'
 import SignOutButton from '../ui/sign-out-button';
@@ -6,30 +13,43 @@ import SignOutButton from '../ui/sign-out-button';
 export default async function Welcome() {
     return (
         // sales, kitchen, supplies, inventory
-        <div className='flex w-screen'>
-            <aside className="w-64 h-screen bg-gray-800 text-white flex flex-col items-start py-8 px-6 border-r border-gray-700">
-                        <div className="text-xl font-bold mb-6 px-4">LeadPro Bakers</div>
-                        <nav className="flex flex-col w-full">
-                            <Link href="/inventory/products" className="w-full mb-3 text-lg font-semibold py-2 px-4 rounded-md hover:bg-gray-700 transition-colors">
-                                Products
-                            </Link>
-                            <Link href="/inventory/sales" className="w-full mb-3 text-lg font-semibold py-2 px-4 rounded-md hover:bg-gray-700 transition-colors">
-                                Sales
-                            </Link>
-                            <Link href="/inventory/kitchen" className="w-full mb-3 text-lg font-semibold py-2 px-4 rounded-md hover:bg-gray-700 transition-colors">
-                                Kitchen
-                            </Link>
-                            <Link href="/inventory/supplies" className="w-full mb-3 text-lg font-semibold py-2 px-4 rounded-md hover:bg-gray-700 transition-colors">
-                                Supplies
-                            </Link>
-                        </nav>
+        <div className='flex w-screen flex-col md:flex-row'>
+            <aside className=" w-full h-20 md:w-64 md:h-screen bg-gray-800 text-white flex flex-row md:flex-col items-start py-8 px-6 border-r border-gray-700">
+                <div className="text-xl font-bold mb-6 px-4 hidden md:block">LeadPro Bakers</div>
+                <nav className="flex flex-row md:flex-col w-full gap-2">
+                    <Link href="/welcome" className="w-full mb-3 flex gap-2 text-lg font-semibold py-2 px-4 rounded-md hover:bg-gray-700 transition-colors">
+                        <HomeIcon className="w-6 h-6 mr-2" />
+                        <span className="hidden md:block">Products</span>
+
+                    </Link>
+                    <Link href="/inventory/products" className="w-full mb-3 flex gap-2 text-lg font-semibold py-2 px-4 rounded-md hover:bg-gray-700 transition-colors">
+                        <ShoppingCartIcon className="w-6 h-6 mr-2" />
+                        <span className="hidden md:block">Products</span>
+
+                    </Link>
+                    <Link href="/inventory/sales" className="w-full mb-3 flex gap-2 text-lg font-semibold py-2 px-4 rounded-md hover:bg-gray-700 transition-colors">
+                        <CurrencyDollarIcon className="w-6 h-6 mr-2" />
+                        <span className="hidden md:block">Sales</span>
+
+                    </Link>
+                    <Link href="/inventory/kitchen" className="w-full mb-3 text-lg flex gap-2 font-semibold py-2 px-4 rounded-md hover:bg-gray-700 transition-colors">
+                        <BuildingStorefrontIcon className="w-6 h-6 mr-2" />
+                        <span className="hidden md:block">Kitchen</span>
+
+                    </Link>
+                    <Link href="/inventory/supplies" className="w-full mb-3 flex gap-2 text-lg font-semibold py-2 px-4 rounded-md hover:bg-gray-700 transition-colors">
+                        <ShoppingBagIcon className="w-6 h-6 mr-2" />
+                        <span className="hidden md:block">Supplies</span>
+
+                    </Link>
+                </nav>
             </aside>
             <div className='flex flex-col min-h-screen flex-auto'>
                 < div className="flex justify-center items-center w-full h-16 text-4xl relative text-black" >
                     <h1>Leadpro Bakers</h1>
                     <SignOutButton />
                 </div >
-                <div className='flex flex-row gap-4 justify-center items-center w-full h-full'>
+                <div className='flex flex-col md:flex-row gap-4 justify-center items-center w-full h-full'>
                     <div className="max-w-sm bg-white rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
                         <div className="relative w-full h-48">
                             <Image
