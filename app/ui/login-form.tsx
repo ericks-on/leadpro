35cd2e62b0c7c928f25ba10d6d2441bee80ade03
Erickson Mbuvi <mutisyaerickson@gmail.com>
2024-08-23 11:31:16 +0300

    login button

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -22,7 +22,13 @@ export default function LoginForm() {
                 <input type="password" id="password" name="password" />
             </div>
             <div className="flex justify-center">
-                <button className="bg-blue-500 text-white px-4 py-2 rounded-lg" aria-disabled={isPending}>Login</button>
+                {isPending ? (
+                    <svg className="animate-spin h-5 w-5 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    </svg>
+                    ): (
+                    <button className="bg-blue-500 text-white px-4 py-2 rounded-lg" aria-disabled={isPending}>Login</button>
+                    )
+                }
             </div>
             {errorMessage && (
                 <>
